5cb07e76984b977922871f48a662f5e9c4edf76b
Prahlad Rao <prahladskingdom@hotmail.com>
2021-02-09 23:14:18 +0530

    Controller - response consumption example

--- src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
+++ src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
@@ -34,8 +34,7 @@ public class PricePlanComparatorController {
     @GetMapping("/compare-all/{smartMeterId}")
     public ResponseEntity<Map<String, Object>> calculatedCostForEachPricePlan(@PathVariable String smartMeterId) {
         String pricePlanId = accountService.getPricePlanIdForSmartMeterId(smartMeterId);
-        Optional<Map<String, BigDecimal>> consumptionsForPricePlans =
-                pricePlanService.getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId);
+        Optional<Map<String, BigDecimal>> consumptionsForPricePlans = getStringBigDecimalMap(smartMeterId);
 
         if (validateConsumptionsForPricePlans(consumptionsForPricePlans)) return ResponseEntity.notFound().build();
 
@@ -55,8 +54,7 @@ public class PricePlanComparatorController {
     @GetMapping("/recommend/{smartMeterId}")
     public ResponseEntity<List<Map.Entry<String, BigDecimal>>> recommendCheapestPricePlans(@PathVariable String smartMeterId,
                                                                                            @RequestParam(value = "limit", required = false) Integer limit) {
-        Optional<Map<String, BigDecimal>> consumptionsForPricePlans =
-                pricePlanService.getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId);
+        Optional<Map<String, BigDecimal>> consumptionsForPricePlans = getStringBigDecimalMap(smartMeterId);
 
         if (validateConsumptionsForPricePlans(consumptionsForPricePlans)) return ResponseEntity.notFound().build();
 
@@ -78,4 +76,59 @@ public class PricePlanComparatorController {
         }
         return false;
     }
+
+    private Optional<Map<String, BigDecimal>> getStringBigDecimalMap(String smartMeterId) {
+        return pricePlanService.getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId);
+    }
+
+
+
+    /*
+    Consumer being UI; implementation before
+
+    Javascript:
+
+    function printName(name) {
+      var ra = {
+        "price-plan-2": 13.824,
+        "price-plan-1": 27.648,
+        "price-plan-0": 138.24
+      };
+
+      var myPlan = {"pricePlanId": "price-plan-0"};
+
+      console.log(ra);
+
+      console.log(Object.keys(ra));
+      console.log(Object.values(ra));
+
+      console.log('My Plan');
+      console.log(Object.keys(ra).find(
+          k => k == myPlan.pricePlanId
+      ));
+
+    }
+
+
+    after structured:
+    function printName(name) {
+      var ra =
+      {
+          "plans" : [
+            {"planName": "price-plan-2", "price": 13.824, "myPlan": false},
+            {"planName": "price-plan-1", "price": 27.648, "myPlan": false},
+            {"planName": "price-plan-0", "price": 138.24, "myPlan": true}
+          ]
+      }
+
+      console.log(ra);
+
+
+      console.log(ra.plans.find(
+          p => p.myPlan === true
+      ));
+
+    }
+     */
+
 }
